404bf22f4ac387b7e5d1838adc96acee2690f14a
Chirurgus <sasha.sashaxl@gmail.com>
2018-08-22 15:53:30 +0200

    Add rest_framework to INSTALLED_APPS

--- cookbox/settings.py
+++ cookbox/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'cb_database.apps.CbDatabaseConfig',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
