199b87f788c91ca1f9659e19ee9ac39b9d72679d
Chirurgus <sasha.sashaxl@gmail.com>
2018-08-22 15:53:10 +0200

    Add routing to rest api views

--- cookbox/urls.py
+++ cookbox/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework import routers
+from cb_database.views import RecipeViewSet
+
+router = routers.DefaultRouter()
+router.register(r'recipes', RecipeViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'^', include('router.urls')),
+    path(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))    
 ]
