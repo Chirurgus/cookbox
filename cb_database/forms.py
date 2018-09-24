f358362cc0abed7a09067a738f3511eff3bdc949
Chirurgus <you@example.com>
2018-09-24 22:01:00 +0200

    Add agregate RecipeForm

--- cb_database/forms.py
+++ cb_database/forms.py
@@ -1,4 +1,5 @@
 from django.forms import *
+from cb_database.nested_form import nestedformset_factory
 from cb_database.models import *
 
 class RecipeForm(ModelForm):
@@ -42,10 +43,36 @@ class RecipeNoteForm(ModelForm):
         exclude = ['id']
 
 
-IngredientGroupFormset =inlineformset_factory(Recipe, IngredientGroup, form= IngredientGroupForm, extra= 0)
-IngredientFormset = inlineformset_factory(IngredientGroup, Ingredient, form= IngredientForm, extra=0)
 InstructionFormset = inlineformset_factory(Recipe, Instruction, form= InstructionForm, extra=0)
+IngredientFormset = inlineformset_factory(IngredientGroup, Ingredient, form= IngredientForm, extra=0)
 TagFormset = inlineformset_factory(Recipe, Tag, form= TagForm, extra= 0)
 RecipeNoteFormset = inlineformset_factory(Recipe, RecipeNote, form= RecipeNoteForm, extra= 0)
 IngredientNoteFormset = inlineformset_factory(Ingredient, IngredientNote, form= IngredientNoteForm, extra= 0)
 InstructionNoteFormset = inlineformset_factory(Instruction, InstructionNote, form= InstructionNoteForm, extra= 0)
+IngredientGroupFormset = nestedformset_factory(Recipe, IngredientGroup, IngredientFormset, extra= 0)
+#IngredientGroupFormset =inlineformset_factory(Recipe, IngredientGroup, extra= 0)
+
+class RecipeCompleteForm():
+    RECIPE_FORM = 'recipe_form'
+    INGREDIENT_GROUPS = 'ingredient_group_forms'
+    INSTRUCTIONS = 'instruction_forms'
+    NOTES = 'note_forms'
+    TAGS = 'tag_forms'
+
+    def __init__(self, data=None, instance=None, *args, **kwargs):
+        self.forms = {self.RECIPE_FORM : RecipeForm(data= data, instance= instance),
+                      self.INGREDIENT_GROUPS : IngredientGroupFormset(data= data, instance= instance, prefix= 'ingredient_groups'),
+                      self.INSTRUCTIONS : InstructionFormset(data= data, instance= instance, prefix= 'instructions'),
+                      self.NOTES : RecipeNoteFormset(data= data, instance= instance, prefix= 'notes'),
+                      self.TAGS : TagFormset(data= data, instance= instance, prefix= 'tags') }
+
+
+    def save(self):
+        for key,form in self.forms.items():
+            form.save()
+
+    def is_valid(self):
+        valid = True
+        for key, form in self.forms.items():
+            valid = form.is_valid() and valid 
+        return valid
