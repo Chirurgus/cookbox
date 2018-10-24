13dcbdfdf1b6890a75702ca155090c3c1b2f1bb2
Chirurgus <you@example.com>
2018-10-24 10:16:35 +0200

    Move static fiels settings, and remove extra coma

--- cookbox/settings.py
+++ cookbox/settings.py
@@ -65,6 +65,21 @@ SECURE_BROWSER_XSS_FILTER = True
 
 X_FRAME_OPTIONS = 'DENY'
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.1/howto/static-files/
+
+LOGIN_URL = reverse_lazy('login')
+LOGIN_REDIRECT_URL = reverse_lazy('recipe-list')
+
+# Path for static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files'),
+STATIC_URL = "/static_files/"
+
+# Path for Images in the datbase
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images'),
+MEDIA_URL = "/images/"
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -165,16 +180,3 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
-
-LOGIN_URL = reverse_lazy('login')
-LOGIN_REDIRECT_URL = reverse_lazy('recipe-list')
-
-# Path for static files
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_files'),
-STATIC_URL = "/static_files/"
-
-# Path for Images in the datbase
-MEDIA_ROOT = os.path.join(BASE_DIR, 'images'),
-MEDIA_URL = "/images/"
