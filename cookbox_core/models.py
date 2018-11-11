64140d8329867b06448c184cfceba7389708fe0e
Chirurgus <you@example.com>
2018-11-11 13:42:28 +0100

    Make unit in ingredints optional

--- cookbox_core/models.py
+++ cookbox_core/models.py
@@ -39,7 +39,7 @@ class IngredientGroup(models.Model):
 
 class Ingredient(models.Model):
     group = models.ForeignKey(IngredientGroup, on_delete=models.CASCADE, related_name= "ingredients")
-    unit = models.CharField(max_length=CHAR_FIELD_MAX_LEN)
+    unit = models.CharField(max_length=CHAR_FIELD_MAX_LEN, blank=True)
     quantity = models.DecimalField(max_digits=DECIMAL_FIELD_MAX_DIGITS,decimal_places=DECIMAL_FIELD_DPLACES)
     description = models.CharField(max_length=CHAR_FIELD_MAX_LEN, default= "")
     usda_code = models.PositiveIntegerField(null= True, blank= True)
