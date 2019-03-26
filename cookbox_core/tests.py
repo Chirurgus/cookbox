b39ef26bd6e035554c8241e76dc7280ffe8010b7
Chirurgus <sasha.sashaxl@gmail.com>
2019-03-26 21:09:28 +0100

    Add setup method to cookbox_core tests

--- cookbox_core/tests.py
+++ cookbox_core/tests.py
@@ -3,9 +3,46 @@
 
 from django.test import TestCase
 
-from cookbox_core.models import Recipe,IngredientGroup, Ingredient
+from cookbox_core.models import (
+    Recipe,
+    IngredientGroup,
+    Ingredient,
+    Instruction,
+)
 
 class IngredientGroupModelTest(TestCase):
-    def test_recipe_foreign_key_not_null(self):
-        for ingredient_group in IngredientGroup.objects.all():
-            self.assertIsNotNone(ingredient_group.recipe)
+    def setUp(self):
+        recipe = Recipe(name="Test recipe",
+               description= "A test recipe",
+               unit_time= "minutes",
+               total_time= 30,
+               preparation_time= 10,
+               cook_time= 10,
+               unit_yield= "test recipe",
+               total_yield= 1,
+               serving_size= 1,
+               source= "me"
+        )
+        recipe.save()
+
+        ing_group = IngredientGroup(
+            recipe= recipe,
+            name= "an test ingredient group",
+            position= 0
+        )
+
+        for i in seq(10):
+            Ingredient(
+                group= ing_roup,
+                unit= "a unit",
+                quantity= i,
+                description= "a test ingredient",
+                position= i
+            )
+        
+        for i in seq(10):
+            Instructions(
+                recipe= recipe,
+                instruction= "step" + str(i),
+                position= i
+            )
