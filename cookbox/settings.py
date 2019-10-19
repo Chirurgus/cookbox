d4de15c84ff4703852f3f5bf93ab8a176adc3ff3
Chirurgus <sasha.sashaxl@gmail.com>
2019-10-19 13:46:56 +0200

    Activate the my authmiddleware

--- cookbox/settings.py
+++ cookbox/settings.py
@@ -109,6 +109,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'cookbox_webui.middleware.AuthRequiredMiddleware',
 ]
 
 ROOT_URLCONF = 'cookbox.urls'
