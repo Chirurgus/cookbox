5d8a9b843681700736451e8a272523e0cee15869
Chirurgus <sasha.sashaxl@gmail.com>
2019-10-27 20:55:20 +0100

    Save scraped recipes inside a transactions

--- cookbox_scraper/__init__.py
+++ cookbox_scraper/__init__.py
@@ -1,5 +1,7 @@
 import re
 
+from django.db import transaction
+
 from cookbox_core.models import Recipe
 
 from .allrecipes import AllRecipes
@@ -59,29 +61,30 @@ def scrape(url):
     """
     scraper = scrape_me(url)
 
-    recipe = Recipe.objects.create(name = scraper.title(),
-                                   description = scraper.description(),
-                                   unit_time = scraper.time_unit(),
-                                   total_time = scraper.time()[0],
-                                   preparation_time = scraper.time()[1],
-                                   cook_time = scraper.time()[2],
-                                   unit_yield = scraper.yield_unit(),
-                                   total_yield = scraper.yields()[0],
-                                   serving_size = scraper.yields()[1],
-                                   source = url)
-    for pos_group, ing_group in enumerate(scraper.ingredients()):
-        group = recipe.ingredient_groups.create(name = ing_group[0], position = pos_group)
-        for idx, ing in enumerate(ing_group[1]):
-            group.ingredients.create(
-                quantity = ing[0],
-                unit = ing[1],
-                description= ing[2],
-                position= idx)
+    with transaction.atomic():
+        recipe = Recipe.objects.create(name = scraper.title(),
+                                    description = scraper.description(),
+                                    unit_time = scraper.time_unit(),
+                                    total_time = scraper.time()[0],
+                                    preparation_time = scraper.time()[1],
+                                    cook_time = scraper.time()[2],
+                                    unit_yield = scraper.yield_unit(),
+                                    total_yield = scraper.yields()[0],
+                                    serving_size = scraper.yields()[1],
+                                    source = url)
+        for pos_group, ing_group in enumerate(scraper.ingredients()):
+            group = recipe.ingredient_groups.create(name = ing_group[0], position = pos_group)
+            for idx, ing in enumerate(ing_group[1]):
+                group.ingredients.create(
+                    quantity = ing[0],
+                    unit = ing[1],
+                    description= ing[2],
+                    position= idx)
 
-    for idx, instruction in enumerate(scraper.instructions()):
-        recipe.instructions.create(instruction = instruction, position= idx)
-    
-    return recipe
+        for idx, instruction in enumerate(scraper.instructions()):
+            recipe.instructions.create(instruction = instruction, position= idx)
+        
+        return recipe
 
 def supported_hosts():
     '''
