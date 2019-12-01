24a5f7715d43074bf694b492d8d03cfa2fd061f0
Chirurgus <sasha.sashaxl@gmail.com>
2019-12-01 23:54:17 +0100

    Remove old Glossary migrations + typo

--- cookbox_glossary/models.py
+++ cookbox_glossary/models.py
@@ -15,7 +15,7 @@ class GlossaryArticle(models.Model):
 
 class GlossaryEntry(models.Model):
     term = models.CharField(max_length=CHAR_FIELD_MAX_LEN_MEDIUM, null=False, blank=False)
-    article = models.ForeignKey(GlossaryArticle, on_delete=models.CASCADE, related_name="entries").
+    article = models.ForeignKey(GlossaryArticle, on_delete=models.CASCADE, related_name="entries")
 
     def save(self, *args, **kwargs):
         self.term = self.term.lower()
