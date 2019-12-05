f4eb027f661dae6f61d6d886b87d1185fdf2f1ee
Chirurgus <sasha.sashaxl@gmail.com>
2019-12-05 10:49:48 +0100

    Add urls to Seasons

---
+++ cookbox_seasons/urls.py
@@ -0,0 +1,18 @@
+# Created by Oleksandr Sorochynskyi
+# On 05/12/2019
+
+from django.urls import path
+
+from .views import (
+    SeasonsListView,
+    SeasonsItemCreateView,
+    SeasonsItemEditView,
+    SeasonsItemDeleteView,
+)
+
+urlpatterns = [
+    path('', SeasonsListView.as_view(), name="seasons"),
+    path('item/create/', SeasonsItemCreateView.as_view(), name="seasons-create"),
+    path('item/<slug:pk>/edit/', SeasonsItemEditView.as_view(), name="seasons-edit"),
+    path('item/<slug:pk>/delete/', SeasonsItemDeleteView.as_view(), name="seasons-delete"),
+]
