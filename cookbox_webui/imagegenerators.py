41ea012b1b2c016cf8dd7aa04d4ff4a225398f15
Chirurgus <sasha.sashaxl@gmail.com>
2020-06-22 11:44:10 +0200

    Change how notes are rendered

--- cookbox_webui/imagegenerators.py
+++ cookbox_webui/imagegenerators.py
@@ -1,21 +1,26 @@
 from imagekit import ImageSpec, register
-from imagekit.processors import ResizeToFill
+from imagekit.processors import SmartResize
 
 class ThumbnailLarge(ImageSpec):
-    processors = [ResizeToFill(900, 600)]
+    processors = [SmartResize(900, 600)]
     format = 'JPEG'
     options = {'quality': 85}
 
 class ThumbnailMedium(ImageSpec):
-    processors = [ResizeToFill(600, 400)]
+    processors = [SmartResize(600, 400)]
     format = 'JPEG'
     options = {'quality': 85}
 
 class ThumbnailSmall(ImageSpec):
-    processors = [ResizeToFill(200, 133)]
+    processors = [SmartResize(200, 133)]
     format = 'JPEG'
     options = {'quality': 70}
+
+class ThumbnailNoPre(ImageSpec):
+    format = 'JPEG'
+    options = {'quality': 85}
 
 register.generator('cookbox_webui:thumbnail_large', ThumbnailLarge)
 register.generator('cookbox_webui:thumbnail_medium', ThumbnailMedium)
 register.generator('cookbox_webui:thumbnail_small', ThumbnailSmall)
+register.generator('cookbox_webui:thumbnail_no_pre', ThumbnailNoPre)
